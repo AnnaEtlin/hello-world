0dc07d0b084f9dd91da1b0ea5c5e614b166a86ee
Anna Etlin <annaetlin@gmail.com>
2016-12-11 21:44:47 -0800

    :q esc

---
+++ My First Function.R
@@ -0,0 +1,21 @@
+add2 <- function(x,y) {
+        x+y
+}
+
+above10 <-function(z){
+      use <- z>10
+      z[use]
+}
+
+above <-function(z, n=12) {
+      use <- z>n
+      z[use]
+}
+columnmean <-function(y, removeNA=TRUE){
+    nc <-ncol(y)
+    means <-numeric(nc)
+    for(i in 1:nc){
+        means[i] <-mean(y[,i], na.rm=removeNA)
+    }
+    means
+}
